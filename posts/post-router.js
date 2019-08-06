59c6874b93250fc128967476145f704322e3650f
Greg Jordan <gregljordan@gmail.com>
2019-08-06 17:54:18 -0400

    Built server and basic router file

---
+++ posts/post-router.js
@@ -0,0 +1,18 @@
+// Declares or imports the router onto express.
+const router = require('express').Router();
+
+// import the database
+const postComments = require('../data/db');
+
+
+
+
+
+
+
+
+
+
+
+// export default router;
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+// Declaration or import the express router
+const express = require('express');
+
+const postRouter = require('./posts/post-router.js')
+
+// Declare the server and invoke express method
+const server = express();
+
+// Allows you to use the json format
+server.use(express.json());
+
+// Makes the router look and the root of the URL, pass it two parameters, 1. the URL and The router name
+server.use('/api/posts', postRouter)
+
+// It makes the server listen on the port specified and sends a message to the console.
+server.listen(8000, () => console.log('\nServer running on port 8000\n'));
+
