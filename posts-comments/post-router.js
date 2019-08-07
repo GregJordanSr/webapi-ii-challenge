abda205dd2e84abc90d98b26909a8e36c0e27f31
Greg Jordan <gregljordan@gmail.com>
2019-08-07 01:29:26 -0400

    completed the put request

--- posts-comments/post-router.js
+++ posts-comments/post-router.js
@@ -34,14 +34,36 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-
+    
     postComments.findById(id)
-})
+        .then(comment => { 
+            if (id) {
+                first()
+                res.status(200).json(comment) 
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+});
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const postInfo = req.body;
+    const { title, contents } = req.body;
 
-
-
-
+    if (!id) {
+        postComments.update(id, postInfo).then(post => res.status(400).json({ message: "The user with the specified ID does not exist." }))
+    } else if (!title || !contents) {
+        postComments.update(id, postInfo).then(post => res.status(400).json({ message: "Please provide title and content for the posts." }))
+    } else {
+        postComments.update(id, postInfo).then(post => res.status(200).json(post))
+        .catch(error => res.status(500).json({ error: "The user information could not be modified"}))
+    }
+   
+})  
 
 // export default router;
 module.exports = router;
